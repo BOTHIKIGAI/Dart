56af259605a12eb15bcd56af7d334f0914c2adc6
Juan Esteban Cajiao Madero <estebax89@gmail.com>
2023-11-14 23:16:10 -0500

    Primer Hola Mundo con dart

---
+++ 1_Inicio/1_holaMundo.dart
@@ -0,0 +1,23 @@
+// 1_holaMundo.dart //
+//////////////////////////////////////////////////////////////
+
+
+void main() { 
+
+  print('Hello world');
+
+}
+
+// Explicación
+/*
+
+  Objetivo -> Hola mundo
+
+  main()
+  Todo codigo en dart necesita en la parte de arriba la palabara main para que se ejecute
+
+  void
+  Las funciones que no son explicitas en el tipo de dato que retornaran tiene la palabra
+  reservada void
+
+*/
